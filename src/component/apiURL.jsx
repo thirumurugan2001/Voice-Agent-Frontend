6a6ea3f85b5e852d6ab609613100121c0a74a699
thirumurugan2001 <thirusubramaniyan2001@gmail.com>
2026-04-13 18:31:51 +0530

    Update BASE_URL in API_URLS to point to the new deployment URL

--- src/component/apiURL.jsx
+++ src/component/apiURL.jsx
@@ -1,6 +1,6 @@
 
 // const BASE_URL ='http://localhost:8080';
-const BASE_URL ='https://voice-agent-teal-eight.vercel.app';
+const BASE_URL ='https://voice-agent-phi-khaki.vercel.app';
 
 export const API_URLS = {
   CHATBOT: {
