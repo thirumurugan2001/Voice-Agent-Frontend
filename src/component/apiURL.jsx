b2ba909282a5e69126934bcb60ad1685e2ff7afc
thirumurugan2001 <thirusubramaniyan2001@gmail.com>
2026-04-16 14:08:31 +0530

    Update BASE_URL in apiURL.jsx to point to the new deployment URL for the Voice Agent application.

--- src/component/apiURL.jsx
+++ src/component/apiURL.jsx
@@ -1,6 +1,6 @@
 
 // const BASE_URL ='http://127.0.0.1:8000';
-const BASE_URL ='https://voice-agent-phi-khaki.vercel.app';
+const BASE_URL ='https://voice-agent-eta-peach.vercel.app';
 
 export const API_URLS = {
   CHATBOT: {
